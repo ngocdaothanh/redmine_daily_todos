b9875516c966395d6b4c1e775a4e87c756589f5e
tramy <myngongao266@yahoo.co.jp>
2010-05-10 19:47:07 +0900

    Sort entry order by begin's time

--- app/models/daily_todo.rb
+++ app/models/daily_todo.rb
@@ -2,6 +2,6 @@ class DailyTodo < ActiveRecord::Base
   LUNCH = 1*60*60  # 1 hour for lunch
 
   belongs_to :user
-  has_many :daily_todo_entries,  :dependent => :destroy
+  has_many :daily_todo_entries,  :order => 'begin', :dependent => :destroy
   has_many :daily_todo_comments, :dependent => :destroy
 end
